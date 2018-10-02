7634fe3fb1b98f53563c779a2e41bf39d0e91cde
Tin Lam <tin@irrational.io>
2018-10-02 01:15:24 -0500

    Refactor configparser logic
    This patch set refactors the configparser logic so it allows for easier
    ini-file values validation.
    
    Closes: #45
    
    Signed-off-by: Tin Lam <tin@irrational.io>

--- subber/config.py
+++ subber/config.py
@@ -12,78 +12,53 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import configparser
+import jsonschema
 import logging
 
 logger = logging.getLogger(__name__)
 
+schema = {
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "string"
+        },
+        "secret": {
+            "type": "string"
+        },
+        "password": {
+            "type": "string"
+        },
+        "username": {
+            "type": "string"
+        }
+    },
+    "required": ["id", "secret", "password", "username"]
+}
 
-def get_config():
-    """Return dictionary containing Subber config values"""
-    CONFIG_FILE = 'subber.cfg'
 
-    def validate_config(cfg):
-        """Validates the cfg file and throws an exception if config is formatted
-        incorrectly or missing data.
-
-        Keyword arguments:
-        cfg -- the config file to validate
-        """
-        # Required opt groups
-        ID = 'id'
-        SECRET = 'secret'
-        PASSWORD = 'password'
-        USERNAME = 'username'
-        reddit_api_opts = [ID, SECRET, PASSWORD, USERNAME]
-
-        # Required sections
-        REDDIT_API = 'reddit-api'
-        required_sections = {REDDIT_API: reddit_api_opts}
-
-        # Validate required sections contain required opts
-        for section, opts in required_sections.items():
-            for opt in opts:
-                if not config.has_section(section):
-                    logger.critical('Missing required config section "{}" in '
-                                    'Subber config'.format(section))
-
-                    raise RuntimeError('Subber config file not loaded.')
-
-                if not config.has_option(section, opt):
-                    logger.critical('Missing required config option "{}" in '
-                                    'section "{}"'.format(opt, section))
-
-                    raise RuntimeError('Subber config file not loaded.')
-
-    # Load config
+def get_config(config_file="subber.cfg"):
     try:
-        config = configparser.RawConfigParser()
-        with open(CONFIG_FILE) as cf:
-            config.read_file(cf)
-    except configparser.MissingSectionHeaderError:
-        logger.critical('Improper config file format detected. No section '
-                        'headers found')
-
+        parser = configparser.ConfigParser()
+        with open(config_file) as f:
+            parser.read_file(f)
+        result = dict(parser.items("reddit-api"))
+        jsonschema.validate(result, schema)
+        return result
+    except FileNotFoundError as notfound:
+        logger.critical(("Config file not found. Please add a config file %s "
+                        "to the project directory."), config_file)
         raise RuntimeError('Subber config file not loaded.')
-    except Exception:
-        logger.critical('Config file not found. Please add a config file "{}" '
-                        'to the project directory.'.format(CONFIG_FILE))
-
+    except configparser.NoSectionError as nosec:
+        logger.critical("Missing header: %s", nosec.message)
         raise RuntimeError('Subber config file not loaded.')
-
-    # Validate config
-    validate_config(config)
-
-    return config
+    except jsonschema.exceptions.SchemaError as serr:
+        logger.critical("Schema error: %s", serr.message)
+        raise RuntimeError('Subber config file not loaded.')
+    except jsonschema.exceptions.ValidationError as verr:
+        logger.critical("Validation error: %s", verr.message)
+        raise RuntimeError('Subber config file not loaded.')
 
 
 def get_api_config():
-    try:
-        return get_config()['reddit-api']
-    except KeyError as e:
-        logger.critical('Could not load configuration for Reddit API. '
-                        'Verify section "[reddit-api]" exists in Subber '
-                        'config.')
-
-        raise RuntimeError('Could not load configuration for Reddit API. '
-                           'Verify section "[reddit-api]" exists in Subber '
-                           'config.')
+    return get_config()
